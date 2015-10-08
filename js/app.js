8da0f1ec2cd7edf0ebcd57b03d6e7fa9534cc48b
supaheckafresh <neilstrain@gmail.com>
2015-10-07 23:17:23 -0700

    Add files

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 
 var entries = [];
+var liHTML = '<li><p>%data%</p></li>';
 
 function getUserEntries() {
-    var string = prompt("Please enter some text you'd like to save.");
+    var string = prompt('Please enter some text you\'d like to save.');
     entries.push(string);
+
+    displayEntry(string);
 }
 
-getUserEntries();
+function displayEntry(entry) {
+    var ul = $('#entries');
+    if (entry) {
+        var formattedLi = liHTML.replace('%data%', entry);
+        ul.append(formattedLi);
+    }
+}
